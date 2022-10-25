01b0d8f8d4b5455e51ba95c11b4108b22b6334ce
liquorice1548 <2295508804@qq.com>
2022-10-25 19:14:04 +0800

    jzd

--- C/Homework/work.c
+++ C/Homework/work.c
@@ -1488,30 +1488,100 @@
 // }
 
 
-#include <stdio.h>
+//大数相加
+// #include <stdio.h>
+// #include <stdlib.h>
+// #define N 50
 
-int main(void)
-{
-    float x,y;
-    scanf("%f",&x);
-    switch(x<1)
-    {
-        case 0:
-            switch(x>=1&&x<10)
-            {
-                case 0:
-                    y=3*x*x-5;
-                    break;
-                case 1:
-                    y=2*x+1;
-                    break;
-            }
-            break;
-        case 1:
-            y=x;
-            break;
-    }
-    printf("\n%f\n",y);
-    return 0;
-}
+// int ReadNumber(int* p)
+// {   
+//     int t[N]={0};
+//     char ch;
+//     int i=0,len=0;
+//     do
+//     {
+//         ch=getchar();
+//         t[i]=atoi(&ch);
+//         i++;
+//     }while(ch!='\n');
+//     len=i-2;
+//     for(i=i-2;i>=0;i--)
+//     {
+//         p[len-i]=t[i];
+//     }
+//     return len;
+// }
 
+// int Calculating(int* a,int* b,int* c,int x,int y)
+// {
+//     int m=(x>=y?x:y);
+//     int i,flag=0;
+//     for(i=0;i<=m+1;i++)
+//     {
+//         c[i]=c[i]+a[i]+b[i];
+//         if(c[i]>=10)
+//         {
+//             c[i]=c[i]-10;
+//             c[i+1]=c[i+1]+1;
+//         }
+//     }
+//     for(i=N;i>=0;i--)
+//     {
+//         if(c[i]!=0)
+//         {
+//             flag=1;
+//             return i+1;
+//         }    
+//     }
+//     if(flag==0)
+//     {
+//         return 1;
+//     }
+// }
+
+// int main(void)
+// {   
+//     int n,x,y;
+//     int a[N]={0};
+//     int b[N]={0};
+//     int c[N+1]={0};
+//     x=ReadNumber(a);
+//     y=ReadNumber(b);
+//     n=Calculating(a,b,c,x,y);
+//     for(int i=n-1;i>=0;i--)
+//     {
+//         printf("%d",c[i]);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+
+//字符串数字提取
+// #include <stdio.h>
+// #include <ctype.h>
+// #include <stdlib.h>
+// #define N 100
+
+// int main(void)
+// {
+//     char str[N]={0};
+//     int t;
+//     char* pstr=str;
+//     printf("Input:\n");
+//     scanf("%s",str);
+//     while(*pstr!='\0')
+//     {
+//         if(isdigit(*pstr))
+//         {
+//             t=atoi(pstr);
+//             printf("%10d\n",t);
+//             while(isdigit(*pstr))
+//             {
+//                 pstr++;
+//             }
+//         }
+//         pstr++;
+//     }
+//     return 0;
+// }
--- C/RPN/main.c
+++ C/RPN/main.c
@@ -8,44 +8,72 @@
 #define N 20
 
 typedef struct node
-{
-    int ival;
+{  
+   int type;
+   union
+   {
+      int ival;
+      double dval;
+   }dat;
 }NodeType;
 typedef struct stack
 {
-    NodeType data[N];
-    int top;            //控制栈顶
+   NodeType data[N];
+   int top;            //控制栈顶
 }STACK;                 //栈的顺序存储
 
 void Push(STACK* stack,NodeType data);
 NodeType Pop(STACK* stack);
-NodeType OpInt(int d1,int d2,int op);
+NodeType Op(NodeType d1,NodeType d2,int op);
 NodeType OpData(NodeType* d1,NodeType* d2,int op);
 
 int main(void)
 {
-    char word[N];
-    NodeType d1,d2,d3;
-    STACK stack;
-    stack.top=0;    //初始化栈顶
+   char word[N];
+   NodeType d1,d2,d3;
+   int i;
+   STACK stack;
+   stack.top=0;    //初始化栈顶
     
-    while(scanf("%s",word)==1&&word[0]!='#')    //以空格为分隔符输入逆波兰表达式，以#结束
-    {
-        if(isdigit(word[0]))    //若为数字，则转化为整形后压栈
-        {
-            d1.ival=atoi(word); //将word转换为整型数据
+   while(scanf("%s",word)==1&&word[0]!='#')    //以空格为分隔符输入逆波兰表达式，以#结束
+   {
+         if(isdigit(word[0]))    //若为数字，则转化为整形后压栈
+         {
+            d1.type=0;
+            for(i=0;i<strlen(word);i++)
+            {
+               if(word[i]=='.')
+               {
+                  d1.type=1;
+               }
+            }
+            if(d1.type==0)
+            {
+               d1.dat.ival=atoi(word); //将word转换为整型数据
+            }
+            else
+            {
+               d1.dat.dval=atof(word);
+            }
             Push(&stack,d1);
-        }
-        else                    //否则弹出两个操作数，执行相应的运算后再将结果压栈
-        {
+         }
+         else                    //否则弹出两个操作数，执行相应的运算后再将结果压栈
+         {
             d2=Pop(&stack);
             d1=Pop(&stack);
             d3=OpData(&d1,&d2,word[0]); //执行运算
             Push(&stack,d3);            //运算结果压入堆栈
-        }
-    }
-    d1=Pop(&stack);     //弹出栈顶保存的最终计算结果
-    printf("%d\n",d1.ival);
+         }
+   }
+   d1=Pop(&stack);     //弹出栈顶保存的最终计算结果
+   if(d1.type==0)
+   {
+      printf("%d\n",d1.dat.ival);
+   }
+   else
+   {
+      printf("%f\n",d1.dat.dval);
+   }
     return 0;
 }
 
@@ -61,30 +89,94 @@ NodeType Pop(STACK* stack)      //弹出栈顶数据并返回
     return stack->data[stack->top];
 }
 
-NodeType OpInt(int d1,int d2,int op)    //对数据d1和d2执行运算op，并返回计算结果
+NodeType Op(NodeType d1,NodeType d2,int op)    //对数据d1和d2执行运算op，并返回计算结果
 {
     NodeType res;
-    switch(op)
+    if(d1.type==0&&d2.type==0)
     {
-        case '+':
-            res.ival=d1+d2;
-            break;
-        case '-':
-            res.ival=d1-d2;
-            break;
-        case '*':
-            res.ival=d1*d2;
-            break;
-        case '/':
-            res.ival=d1/d2;
-            break;
-    }
-    return res;
+      res.type=0;
+      switch(op)
+      {
+         case '+':
+               res.dat.ival=d1.dat.ival+d2.dat.ival;
+               break;
+         case '-':
+               res.dat.ival=d1.dat.ival-d2.dat.ival;
+               break;
+         case '*':
+               res.dat.ival=d1.dat.ival*d2.dat.ival;
+               break;
+         case '/':
+               res.dat.ival=d1.dat.ival/d2.dat.ival;
+               break;
+      }
+   }
+   else if(d1.type==0&&d2.type==1)
+   {
+      res.type=1;
+      switch(op)
+      {
+         case '+':
+               res.dat.dval=d1.dat.ival+d2.dat.dval;
+               break;
+         case '-':
+               res.dat.dval=d1.dat.ival-d2.dat.dval;
+               break;
+         case '*':
+               res.dat.dval=d1.dat.ival*d2.dat.dval;
+               break;
+         case '/':
+               res.type=0;
+               res.dat.ival=d1.dat.ival/(int)d2.dat.dval;
+               break;
+      }
+   }
+   else if(d1.type==1&&d2.type==0)
+   {
+      res.type=1;
+      switch(op)
+      {
+         case '+':
+               res.dat.dval=d1.dat.dval+d2.dat.ival;
+               break;
+         case '-':
+               res.dat.dval=d1.dat.dval-d2.dat.ival;
+               break;
+         case '*':
+               res.dat.dval=d1.dat.dval*d2.dat.ival;
+               break;
+         case '/':
+               res.type=0;
+               res.dat.ival=d1.dat.dval/d2.dat.ival;
+               break;
+      }
+   }
+   else
+   {
+      res.type=1;
+      switch(op)
+      {
+         case '+':
+               res.dat.dval=d1.dat.dval+d2.dat.dval;
+               break;
+         case '-':
+               res.dat.dval=d1.dat.dval-d2.dat.dval;
+               break;
+         case '*':
+               res.dat.dval=d1.dat.dval*d2.dat.dval;
+               break;
+         case '/':
+               res.type=0;
+               res.dat.ival=(int)d1.dat.dval/(int)d2.dat.dval;
+               break;
+      }
+   }
+   return res;
 }
 
 NodeType OpData(NodeType* d1,NodeType* d2,int op)   //对d1和d2执行运算op，并返回计算结果
 {
     NodeType res;
-    res=OpInt(d1->ival,d2->ival,op);
+    res=Op(*d1,*d2,op);
     return res;
 }
