03635deb7c88734e672dc458a560fdbdf5d521a8
liquorice1548 <2295508804@qq.com>
2022-11-25 11:23:04 +0800

    123

---
+++ Python/work.py
@@ -0,0 +1,30 @@
+def get_k_run_starter(n, k):
+    """
+    >>> get_k_run_starter(123444345, 0) # example from description
+    3
+    >>> get_k_run_starter(123444345, 1)
+    4
+    >>> get_k_run_starter(123444345, 2)
+    4
+    >>> get_k_run_starter(123444345, 3)
+    1
+    >>> get_k_run_starter(123412341234, 1)
+    1
+    >>> get_k_run_starter(1234234534564567, 0)
+    4
+    >>> get_k_run_starter(1234234534564567, 1)
+    3
+    >>> get_k_run_starter(1234234534564567, 2)
+    2
+    """
+    i = 0
+    final = None
+    while i < k + 1:
+        while n % 10 > (n % 100 - n % 10) // 10 and n > 9:
+            n = n // 10
+        final = n % 10
+        i = i + 1
+        n = n // 10
+    return final
+
+
