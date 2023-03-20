99caaae87ebff8e191539ff4e5f3750968165f83
liquorice1548 <2295508804@qq.com>
2023-03-20 20:40:51 +0800

    v1

---
+++ Java/DLList.java
@@ -0,0 +1,5 @@
+package Java;
+//双向链表
+public class DLList {
+    
+}
--- Java/SLList.java
+++ Java/SLList.java
@@ -1,24 +1,60 @@
 package Java;
+//单向链表
+public class SLList<DataType> {
+    public class Node {
+        public DataType item;
+        public Node next;
+    
+        public Node(DataType i, Node n) {
+            item = i;
+            next = n;
+        }
+    }
+    
+    private Node sentinel;
+    private Node last;
+    private int size;
 
-public class SLList {
-    public IntNode first;
-
-    public SLList(int x) {
-        first = new IntNode(x, null);
+    public SLList() {
+        sentinel = new Node(null, null);
+        size = 0;
     }
 
-    public void addFirst(int x) {
-        first = new IntNode(x, first);
+    public SLList(DataType x) {
+        sentinel = new Node(null, null);
+        sentinel.next = new Node(x, null);
+        last = sentinel.next;
+        size = 1;
     }
 
-    public int getFirst() {
-        return first.item;
+    public void addFirst(DataType x) {
+        size ++;
+        sentinel.next = new Node(x, sentinel.next);
+    }
+
+    public DataType getFirst() {
+        return sentinel.next.item;
+    }
+
+    public void addLast(DataType x) {
+        size++;
+        last.next = new Node(x, null);
+        last = last.next;
+    }
+
+    public DataType getLast() {
+        return last.item;
+    }
+
+    public int size() {
+        return size;
     }
 
     public static void main(String[] args) {
-        SLList L = new SLList(3);
+        SLList<Integer> L = new SLList<>(3);
         L.addFirst(2);
         L.addFirst(1);
-        L.addFirst(0);
+        L.addLast(0);
+        System.out.print(L.getLast());
     }
 }
