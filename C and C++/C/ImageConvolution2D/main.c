89e3cb4a0811d2b8d881ba56fa4dc723496e18bd
liquorice1548 <2295508804@qq.com>
2023-04-30 11:03:12 +0800

    changes

--- C and C++/C/ImageConvolution2D/main.c
+++ C and C++/C/ImageConvolution2D/main.c
@@ -3,135 +3,135 @@
 #include <assert.h>
 #include "cv.h"
 
-BMPImage* LoadBMPImage(char* path);
-BMPImage* Convolution2D(BMPImage* img,float const kernel[3][3]);
-bool SaveBMPImage(char* path, BMPImage* bmpImg);
+BMPImage *LoadBMPImage(char *path);
+BMPImage *Convolution2D(BMPImage *img, float const kernel[3][3]);
+bool SaveBMPImage(char *path, BMPImage *bmpImg);
 
 int main(void)
 {
-    BMPImage* img = LoadBMPImage("xm.bmp");
-    BMPImage* convdImg;
-    float const kernel[3][3] = {{-1./8 , -1./8 , -1./8},
-                                { -1./8 , 1 , -1./8 },
-                                {-1./8, -1./8, -1./8 }};
+    BMPImage *img = LoadBMPImage("xm.bmp");
+    BMPImage *convdImg;
+    float const kernel[3][3] = {{-1. / 8, -1. / 8, -1. / 8},
+                                {-1. / 8, 1, -1. / 8},
+                                {-1. / 8, -1. / 8, -1. / 8}};
     convdImg = Convolution2D(img, kernel);
-    if(SaveBMPImage("result.bmp", convdImg))
+    if (SaveBMPImage("result.bmp", convdImg))
     {
         printf("bmp image processed.\n");
     }
     return 0;
 }
 
-
-BMPImage* Convolution2D(BMPImage* img,float const kernel[3][3])
+BMPImage *Convolution2D(BMPImage *img, float const kernel[3][3])
 {
-    int i,j,k,l;
-    float sum=0;
-    unsigned char temp[img->height+2][img->width+2];
-    for(i=0;i<img->height+2;i++)
+    int i, j, k, l;
+    float sum = 0;
+    unsigned char temp[img->height + 2][img->width + 2];
+    for (i = 0; i < img->height + 2; i++)
     {
-        for(j=0;j<img->width+2;j++)
+        for (j = 0; j < img->width + 2; j++)
         {
-            temp[i][j]=0;
+            temp[i][j] = 0;
         }
     }
 
-    for(i=1;i<img->height;i++)
+    for (i = 1; i < img->height; i++)
     {
-        for(j=1;j<img->width;j++)
+        for (j = 1; j < img->width; j++)
         {
-            temp[i][j]=img->imageData[(i-1)*img->width+(j-1)].R;
+            temp[i][j] = img->imageData[(i - 1) * img->width + (j - 1)].R;
         }
     }
-    for(i=1;i<img->height;i++)
+    for (i = 1; i < img->height; i++)
     {
-        for(j=1;j<img->width;j++)
+        for (j = 1; j < img->width; j++)
         {
-            sum=0;
-            for(k=i-1;k<=i+1;k++)
+            sum = 0;
+            for (k = i - 1; k <= i + 1; k++)
             {
-                for(l=j-1;l<=j+1;l++)
+                for (l = j - 1; l <= j + 1; l++)
                 {
-                    sum=sum+temp[k][l]*kernel[k-i+1][l-j+1];
+                    sum = sum + temp[k][l] * kernel[k - i + 1][l - j + 1];
                 }
             }
-            img->imageData[(i-1)*img->width+(j-1)].R=(unsigned char)(sum+0.5f);
+            img->imageData[(i - 1) * img->width + (j - 1)].R = (unsigned char)(sum + 0.5f);
         }
     }
 
-    for(i=1;i<img->height;i++)
+    for (i = 1; i < img->height; i++)
     {
-        for(j=1;j<img->width;j++)
+        for (j = 1; j < img->width; j++)
         {
-            temp[i][j]=img->imageData[(i-1)*img->width+(j-1)].B;
+            temp[i][j] = img->imageData[(i - 1) * img->width + (j - 1)].B;
         }
     }
-    for(i=1;i<img->height;i++)
+    for (i = 1; i < img->height; i++)
     {
-        for(j=1;j<img->width;j++)
+        for (j = 1; j < img->width; j++)
         {
-            sum=0;
-            for(k=i-1;k<=i+1;k++)
+            sum = 0;
+            for (k = i - 1; k <= i + 1; k++)
             {
-                for(l=j-1;l<=j+1;l++)
+                for (l = j - 1; l <= j + 1; l++)
                 {
-                    sum=sum+temp[k][l]*kernel[k-i+1][l-j+1];
+                    sum = sum + temp[k][l] * kernel[k - i + 1][l - j + 1];
                 }
             }
-            img->imageData[(i-1)*img->width+(j-1)].B=(unsigned char)(sum+0.5f);
+            img->imageData[(i - 1) * img->width + (j - 1)].B = (unsigned char)(sum + 0.5f);
         }
     }
 
-    for(i=1;i<img->height;i++)
+    for (i = 1; i < img->height; i++)
     {
-        for(j=1;j<img->width;j++)
+        for (j = 1; j < img->width; j++)
         {
-            temp[i][j]=img->imageData[(i-1)*img->width+(j-1)].G;
+            temp[i][j] = img->imageData[(i - 1) * img->width + (j - 1)].G;
         }
     }
-    for(i=1;i<img->height;i++)
+    for (i = 1; i < img->height; i++)
     {
-        for(j=1;j<img->width;j++)
+        for (j = 1; j < img->width; j++)
         {
-            sum=0;
-            for(k=i-1;k<=i+1;k++)
+            sum = 0;
+            for (k = i - 1; k <= i + 1; k++)
             {
-                for(l=j-1;l<=j+1;l++)
+                for (l = j - 1; l <= j + 1; l++)
                 {
-                    sum=sum+temp[k][l]*kernel[k-i+1][l-j+1];
+                    sum = sum + temp[k][l] * kernel[k - i + 1][l - j + 1];
                 }
             }
-            img->imageData[(i-1)*img->width+(j-1)].G=(unsigned char)(sum+0.5f);
+            img->imageData[(i - 1) * img->width + (j - 1)].G = (unsigned char)(sum + 0.5f);
         }
     }
     return img;
 }
 
-BMPImage* LoadBMPImage(char* path)
+BMPImage *LoadBMPImage(char *path)
 {
-    FILE* pFile;
+    FILE *pFile;
 
     /**0. Init Values**/
-    //BMP Image parts
-    BMPImage* bmpImg;  //Img Content
+    // BMP Image parts
+    BMPImage *bmpImg; // Img Content
     BitMapFileHeader bmpFileHeader;
     BitMapInfoHeader bmpInfoHeader;
-    //BMP Image file info
-    unsigned short fileType;  //Picture type Sign
+    // BMP Image file info
+    unsigned short fileType; // Picture type Sign
     int channels = 3;
     int width = 0;
     int height = 0;
-    //Data Content Parameters
+    // Data Content Parameters
     int offset = 0;
-    PixelBlock pixVal;  //One Pixel Value
+    PixelBlock pixVal; // One Pixel Value
     unsigned char offset_char;
     int i, j;
 
-    bmpImg = (BMPImage*)malloc(sizeof(BMPImage));  //Will return this Variable
+    bmpImg = (BMPImage *)malloc(sizeof(BMPImage)); // Will return this Variable
 
     /**1. File Open**/
     pFile = fopen(path, "rb");
-    if (!pFile){
+    if (!pFile)
+    {
         free(bmpImg);
         return NULL;
     }
@@ -140,10 +140,10 @@ BMPImage* LoadBMPImage(char* path)
     fread(&fileType, sizeof(unsigned short), 1, pFile);
     if (fileType == 0x4D42)
     {
-        //2.1 Read Header
+        // 2.1 Read Header
         fread(&bmpFileHeader, sizeof(BitMapFileHeader), 1, pFile);
 
-        //2.2 Get Picture Info
+        // 2.2 Get Picture Info
         fread(&bmpInfoHeader, sizeof(BitMapInfoHeader), 1, pFile);
         assert(bmpInfoHeader.biBitCount == 24);
         width = bmpInfoHeader.biWidth;
@@ -151,8 +151,8 @@ BMPImage* LoadBMPImage(char* path)
         bmpImg->width = width;
         bmpImg->height = height;
         bmpImg->channels = channels;
-        bmpImg->imageData = (PixelBlock*)malloc(sizeof(PixelBlock)*width*height);
-        offset = (channels*width)%4;    //offset in bmpFile
+        bmpImg->imageData = (PixelBlock *)malloc(sizeof(PixelBlock) * width * height);
+        offset = (channels * width) % 4; // offset in bmpFile
 
         if (offset != 0)
         {
@@ -160,21 +160,21 @@ BMPImage* LoadBMPImage(char* path)
         }
 
         /**Most Important**/
-        //2.3 Reading ImageData
-        for (i=0; i<height; i++)
+        // 2.3 Reading ImageData
+        for (i = 0; i < height; i++)
         {
-            for (j=0; j<width; j++)
+            for (j = 0; j < width; j++)
             {
-                //2.3.1 Read One single Pixel to &pixVal
+                // 2.3.1 Read One single Pixel to &pixVal
                 fread(&pixVal.B, sizeof(unsigned char), 1, pFile);
                 fread(&pixVal.G, sizeof(unsigned char), 1, pFile);
                 fread(&pixVal.R, sizeof(unsigned char), 1, pFile);
-                //2.3.2 Read One single Pixel to &pixVal
-                bmpImg->imageData[(height-1-i)*width+j] = pixVal;
+                // 2.3.2 Read One single Pixel to &pixVal
+                bmpImg->imageData[(height - 1 - i) * width + j] = pixVal;
             }
             if (offset != 0)
             {
-                for (j=0; j<offset; j++)
+                for (j = 0; j < offset; j++)
                 {
                     fread(&offset_char, sizeof(unsigned char), 1, pFile);
                 }
@@ -185,7 +185,7 @@ BMPImage* LoadBMPImage(char* path)
     return bmpImg;
 }
 
-bool SaveBMPImage(char* path, BMPImage* bmpImg)
+bool SaveBMPImage(char *path, BMPImage *bmpImg)
 {
     FILE *pFile;
     unsigned short fileType;
@@ -207,47 +207,47 @@ bool SaveBMPImage(char* path, BMPImage* bmpImg)
     fwrite(&fileType, sizeof(unsigned short), 1, pFile);
 
     assert(bmpImg->channels == 3);
-    rowLen = bmpImg->channels*bmpImg->width;
-    offset = (rowLen)%4;
+    rowLen = bmpImg->channels * bmpImg->width;
+    offset = (rowLen) % 4;
     if (offset != 4)
     {
-        rowLen += 4-offset;
+        rowLen += 4 - offset;
     }
 
-    //Write Header
-    bmpFileHeader.bfSize = bmpImg->height*rowLen + 54;
+    // Write Header
+    bmpFileHeader.bfSize = bmpImg->height * rowLen + 54;
     bmpFileHeader.fReserved1 = 0;
     bmpFileHeader.fReserved2 = 0;
     bmpFileHeader.fOffBits = 54;
     fwrite(&bmpFileHeader, sizeof(BitMapFileHeader), 1, pFile);
 
-    //Write Info
+    // Write Info
     bmpInfoHeader.biSize = 40;
     bmpInfoHeader.biWidth = bmpImg->width;
     bmpInfoHeader.biHeight = bmpImg->height;
     bmpInfoHeader.biPlanes = 1;
     bmpInfoHeader.biBitCount = 24;
     bmpInfoHeader.biCompression = 0;
-    bmpInfoHeader.biSizeImage = bmpImg->height*rowLen;
+    bmpInfoHeader.biSizeImage = bmpImg->height * rowLen;
     bmpInfoHeader.biXPelsPerMeter = 0;
     bmpInfoHeader.biYPelsPerMeter = 0;
     bmpInfoHeader.biClrUsed = 0;
     bmpInfoHeader.biClrImportant = 0;
     fwrite(&bmpInfoHeader, sizeof(BitMapInfoHeader), 1, pFile);
 
-    //Write Image Data
-    for (i=bmpImg->height-1; i>-1; i--)
+    // Write Image Data
+    for (i = bmpImg->height - 1; i > -1; i--)
     {
-        for (j=0; j<bmpImg->width; j++)
+        for (j = 0; j < bmpImg->width; j++)
         {
-            pixVal = bmpImg->imageData[i*bmpImg->width+j];
+            pixVal = bmpImg->imageData[i * bmpImg->width + j];
             fwrite(&pixVal.B, sizeof(unsigned char), 1, pFile);
             fwrite(&pixVal.G, sizeof(unsigned char), 1, pFile);
             fwrite(&pixVal.R, sizeof(unsigned char), 1, pFile);
         }
-        if (offset!=0)
+        if (offset != 0)
         {
-            for (j=0; j<offset; j++)
+            for (j = 0; j < offset; j++)
             {
                 offset_char = 0;
                 fwrite(&offset_char, sizeof(unsigned char), 1, pFile);
@@ -258,11 +258,3 @@ bool SaveBMPImage(char* path, BMPImage* bmpImg)
 
     return true;
 }
-
-
-
-
-
-
-
-
--- C and C++/C/WordCorrecting/main.c
+++ C and C++/C/WordCorrecting/main.c
@@ -4,141 +4,140 @@
 #include <ctype.h>
 #define N 50
 
-int Levenshtein(char* str,char* dst);
-int Min(int a,int b,int c);
+int Levenshtein(char *str, char *dst);
+int Min(int a, int b, int c);
 
 int main(void)
 {
-    FILE* fp1;
-    FILE* fp2;
-    FILE* fp3;
-    int ch,ch1,ch2;
-    int i,n,min=0,cnt=0,f=1,flag=1;
-    char str[N]={0};
-    char dst[N]={0};
-    char temp[N]={0};
-    ch=ch1=ch2=0;
-    fp1=fopen("words.txt","r");
-    fp2=fopen("vocabulary.txt","r");
-    fp3=fopen("words_correct.txt","w");
-    while(!feof(fp1))
+    FILE *fp1;
+    FILE *fp2;
+    FILE *fp3;
+    int ch, ch1, ch2;
+    int i, n, min = 0, cnt = 0, f = 1, flag = 1;
+    char str[N] = {0};
+    char dst[N] = {0};
+    char temp[N] = {0};
+    ch = ch1 = ch2 = 0;
+    fp1 = fopen("words.txt", "r");
+    fp2 = fopen("vocabulary.txt", "r");
+    fp3 = fopen("words_correct.txt", "w");
+    while (!feof(fp1))
     {
-        
+
         do
         {
-            ch=fgetc(fp1);
-            printf("%c",ch);
-            fputc(ch,fp3);
-        } while (!(ch==' '||ch==-1));
+            ch = fgetc(fp1);
+            printf("%c", ch);
+            fputc(ch, fp3);
+        } while (!(ch == ' ' || ch == -1));
 
-        flag=1;
-        
-        while(flag)
+        flag = 1;
+
+        while (flag)
         {
-            for(i=0;i<N;i++)
+            for (i = 0; i < N; i++)
             {
-                str[i]=0;
+                str[i] = 0;
             }
-            i=0;
+            i = 0;
 
             do
             {
-                ch1=fgetc(fp1);
-                if(isalpha((char)ch1))
+                ch1 = fgetc(fp1);
+                if (isalpha((char)ch1))
                 {
-                    str[i]=(char)ch1;
+                    str[i] = (char)ch1;
                     i++;
                 }
-                if((!isprint((char)ch1))&&ch1!='\n'&&ch1!=-1)
+                if ((!isprint((char)ch1)) && ch1 != '\n' && ch1 != -1)
                 {
-                    while(ch1>=127||ch1<=64)
+                    while (ch1 >= 127 || ch1 <= 64)
                     {
                         ch1 = fgetc(fp1);
                     }
                 }
-                if(ch1==' '||ch1=='\n'||ch1=='/'||ch1==-1)
+                if (ch1 == ' ' || ch1 == '\n' || ch1 == '/' || ch1 == -1)
                 {
-                    flag=0;
+                    flag = 0;
                 }
-            }while(flag);
+            } while (flag);
 
-            flag=1;
-            while(!feof(fp2))
+            flag = 1;
+            while (!feof(fp2))
             {
-                for(i=0;i<N;i++)
+                for (i = 0; i < N; i++)
                 {
-                    dst[i]=0;
+                    dst[i] = 0;
                 }
-                i=0;
+                i = 0;
                 do
                 {
-                    ch2=fgetc(fp2);
-                    if(isalpha((char)ch2))
+                    ch2 = fgetc(fp2);
+                    if (isalpha((char)ch2))
                     {
-                        dst[i]=(char)ch2;
+                        dst[i] = (char)ch2;
                     }
                     i++;
-                    if(ch2=='\n'||ch2==' '||ch2==-1)
+                    if (ch2 == '\n' || ch2 == ' ' || ch2 == -1)
                     {
-                        flag=0;
+                        flag = 0;
                     }
-                }while(flag);
+                } while (flag);
 
-                flag=1;
+                flag = 1;
 
-                for(i=0;i<strlen(dst);i++)
+                for (i = 0; i < strlen(dst); i++)
                 {
-                    if(dst[i]==' ')
+                    if (dst[i] == ' ')
                     {
-                        f=0;
+                        f = 0;
                         break;
                     }
                 }
 
-                if(f)
+                if (f)
                 {
                     cnt++;
-                    if(cnt==1)
+                    if (cnt == 1)
                     {
-                        for(i=0;i<N;i++)
+                        for (i = 0; i < N; i++)
                         {
-                            temp[i]=0;
+                            temp[i] = 0;
                         }
-                        min=Levenshtein(str,dst);
-                        strcpy(temp,str);
+                        min = Levenshtein(str, dst);
+                        strcpy(temp, str);
                     }
-                    n= Levenshtein(str,dst);
-                    if(n==0)
+                    n = Levenshtein(str, dst);
+                    if (n == 0)
                     {
-                        strcpy(temp,str);
+                        strcpy(temp, str);
                         goto out;
                     }
-                    else if(min>n)
+                    else if (min > n)
                     {
-                        min=n;
-                        for(i=0;i<N;i++)
+                        min = n;
+                        for (i = 0; i < N; i++)
                         {
-                            temp[i]=0;
+                            temp[i] = 0;
                         }
-                        strcpy(temp,dst);
-                    }                  
-                }   
-            }  
-            out:
-            cnt=0;
-            rewind(fp2);
-            printf("%s",temp);
-            fputs(temp,fp3);
-            if(isprint((char)ch1)||ch1=='\n')
-            {
-                printf("%c",ch1);
-                fputc(ch1,fp3);
+                        strcpy(temp, dst);
+                    }
+                }
             }
-            if(ch1=='\n'||ch1==-1)
+        out:
+            cnt = 0;
+            rewind(fp2);
+            printf("%s", temp);
+            fputs(temp, fp3);
+            if (isprint((char)ch1) || ch1 == '\n')
             {
-                flag=0;
-            }   
-             
+                printf("%c", ch1);
+                fputc(ch1, fp3);
+            }
+            if (ch1 == '\n' || ch1 == -1)
+            {
+                flag = 0;
+            }
         }
     }
     fclose(fp1);
@@ -147,49 +146,49 @@ int main(void)
     return 0;
 }
 
-int Levenshtein(char* str,char* dst)
+int Levenshtein(char *str, char *dst)
 {
-    int distance[strlen(str)+1][strlen(dst)+1];
-    int i,j;
-    for(i=0;i<=strlen(dst);i++)
+    int distance[strlen(str) + 1][strlen(dst) + 1];
+    int i, j;
+    for (i = 0; i <= strlen(dst); i++)
     {
-        distance[0][i]=i;
+        distance[0][i] = i;
     }
-    for(i=0;i<=strlen(str);i++)
+    for (i = 0; i <= strlen(str); i++)
     {
-        distance[i][0]=i;
+        distance[i][0] = i;
     }
-    for(i=1;i<=strlen(str);i++)
+    for (i = 1; i <= strlen(str); i++)
     {
-        for(j=1;j<=strlen(dst);j++)
+        for (j = 1; j <= strlen(dst); j++)
         {
-            if(str[i-1]==dst[j-1])
+            if (str[i - 1] == dst[j - 1])
             {
-                distance[i][j]=Min(distance[i-1][j]+1,distance[i][j-1]+1,distance[i-1][j-1]);
+                distance[i][j] = Min(distance[i - 1][j] + 1, distance[i][j - 1] + 1, distance[i - 1][j - 1]);
             }
             else
             {
-                distance[i][j]=Min(distance[i-1][j]+1,distance[i][j-1]+1,distance[i-1][j-1]+1);
+                distance[i][j] = Min(distance[i - 1][j] + 1, distance[i][j - 1] + 1, distance[i - 1][j - 1] + 1);
             }
         }
     }
     return distance[strlen(str)][strlen(dst)];
 }
 
-int Min(int a,int b,int c)
+int Min(int a, int b, int c)
 {
     int min;
-    if(a>=b)
+    if (a >= b)
     {
-        min=b;
+        min = b;
     }
     else
     {
-        min=a;
+        min = a;
     }
-    if(c<=min)
+    if (c <= min)
     {
-        min=c;
+        min = c;
     }
     return min;
 }
