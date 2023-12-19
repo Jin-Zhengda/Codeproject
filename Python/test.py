cdacfced8e52e85b461e48aa3c3b266c7f9c7e6b
liquorice1548 <2295508804@qq.com>
2023-12-19 16:26:30 +0800

    changes

--- Python/test.py
+++ Python/test.py
@@ -1,21 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-d = np.array([29.360-28.562,29.360-28.560,29.366-28.564,29.362-28.559,29.364-28.564])
-d_me = np.mean(d)
-print(d_me)
-d_me = d_me * 10e-3
-pho = 7800
-g = 9.78
-pho0 = 950
-D = 0.02
-de_t = 0.2
-L = 0.2
+# x = np.array([10, 13, 16, 19, 22, 25, 28, 31])
+# y = np.array([6.36, 5.04, 3.48, 2.36, 1.88, 1.12, 0.82, 0.68])
 
-t = 6.41
+# plt.rc('font', family = 'SimHei')
+# plt.rc('axes', unicode_minus = False)
+# plt.xlabel("距离(cm)")
+# plt.ylabel("电压(Vpp)")
+# plt.plot(x, y)
+# plt.show()
 
-k = (pho - pho0) * d_me  ** 2 * t / (18 * L * (1 + 2.4 * d_me / D))
-print(k)
-
-plt.plot([30, 36, 42, 50, 60], [0.06, 0.027, 0.016, 0.01, 0.007])
-plt.show()
+x = np.array([0.601, 0.598, 0.596, 0.598, 0.598, 0.596])
+y = np.mean(x)
+u = ((1 / 30) * np.sum((x - y) ** 2)) ** 0.5
+print(y)
+print(u)
